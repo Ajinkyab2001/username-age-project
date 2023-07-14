c8ef308482295f6ab15d1422c6ce73870017ecc5
Ajinkyab2001 <ajinkyab2001@gmail.com>
2023-07-15 01:36:55 +0530

    username-age-udemyproject

---
+++ src/components/User/UsersList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./UserList.css";
+import Card from "../UI/Card";
+
+const UsersList = (props) => {
+  return (
+    <Card>
+      <div className="users">
+        <ul>
+          {props.users.map((element) => {
+            return (
+              <li key={new Date().getTime()}>
+                {element.username} (is {element.age} years old)
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </Card>
+  );
+};
+
+export default UsersList;
